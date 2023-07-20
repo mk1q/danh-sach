f2efc9e9572dcc82d99000d7d70fa43c9cfcd0b1
ngo ba kha <anhemdaica123@gmail.com>
2023-07-20 13:28:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//Chương trình in nội dung bức thư
+let person = prompt("Tên người nhận: ", "Joe");
+let p= prompt("Địa điểm: ", "Los Angeles")
+let m= prompt("Thời gian: " ,   " ngày 10 tháng 10 năm 1951")
